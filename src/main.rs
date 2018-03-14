6d91e18333c01fdee42a7adc6363741406aef7da
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-14 16:33:43 -0500

    Directorio de archivos estáticos

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 extern crate nickel;
 
-use nickel::{Nickel, HttpRouter, Request, Response, MiddlewareResult};
+use nickel::{Nickel, HttpRouter, Request, Response, MiddlewareResult, StaticFilesHandler};
 
-fn hello_world<'mw>(_req: &mut Request, res: Response<'mw>) -> MiddlewareResult<'mw> {
+fn index<'mw>(_req: &mut Request, res: Response<'mw>) -> MiddlewareResult<'mw> {
   res.send("Hello World??")
 }
 
 fn main() {
   let mut server = Nickel::new();
-  server.get("**", hello_world);
+  server.utilize(StaticFilesHandler::new("public/"));
+  server.get("/", index);
   server.listen("127.0.0.1:6767").unwrap();
 }
